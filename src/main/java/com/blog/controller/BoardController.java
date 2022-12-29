0b38b6140d74df23056551776dc835b604972b8f
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-12-29 22:22:20 +0900

    feat:(BoardController) 보드 컨트롤러 추가

---
+++ src/main/java/com/blog/controller/BoardController.java
@@ -0,0 +1,16 @@
+package com.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BoardController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+}
+
+
