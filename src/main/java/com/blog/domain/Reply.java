ceadff21fa4ef338059f3da57cc484f0664df0b3
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-12-29 19:52:25 +0900

    feat: (Reply) 댓글 도메인 추가

---
+++ src/main/java/com/blog/domain/Reply.java
@@ -0,0 +1,36 @@
+package com.blog.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Reply {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 200)
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "boardId")
+    private Board board;
+
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private Member member;
+
+    @CreationTimestamp
+    private Timestamp createDate;
+}
