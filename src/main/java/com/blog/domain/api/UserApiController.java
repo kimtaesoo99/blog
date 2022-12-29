7cbe835ee547ae70611d80b8d0dedb2e97f6ab89
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-12-29 22:13:34 +0900

    feat:(UserApiController) 유저 Api 컨트롤러 추가

---
+++ src/main/java/com/blog/domain/api/UserApiController.java
@@ -0,0 +1,31 @@
+package com.blog.domain.api;
+
+
+import com.blog.domain.ROLETYPE;
+import com.blog.domain.User;
+import com.blog.dto.ResponseDto;
+import com.blog.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserApiController {
+
+    @Autowired
+    private UserService userService;
+
+    // Json 데이터를 받으려면 @RequestBo가dy로 받아야함
+    // 회원가입
+    @PostMapping("/auth/joinProc")
+    public ResponseDto<Integer> save(@RequestBody User user) {
+        user.setRole(ROLETYPE.USER);
+        userService.save(user);
+        return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
+        // 자바 오브젝트를 JSON으로 변환하여 전송 (JACKSON)
+    }
+
+
+}
