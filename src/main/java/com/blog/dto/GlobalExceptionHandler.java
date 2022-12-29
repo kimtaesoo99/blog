d996c7ca6ce996c785cc37cd893c677d02769d79
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-12-29 22:21:22 +0900

    feat:(GlobalExceptionHandler) 외외를 잡아서 처리해주는 클래스 추가

---
+++ src/main/java/com/blog/dto/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.blog.dto;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RestController
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = Exception.class)
+    public ResponseDto<String> handleArgumentException(Exception e){
+        return new ResponseDto<String>(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage());
+    }
+}
