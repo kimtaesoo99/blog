90f492ea12b91d9503166bd852b82d07e4a3a001
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-12-30 22:05:49 +0900

    refactor: (BoardRepository) 쿼리문 추가

--- src/main/java/com/blog/repository/BoardRepository.java
+++ src/main/java/com/blog/repository/BoardRepository.java
@@ -1,9 +1,16 @@
 package com.blog.repository;
 
 import com.blog.domain.Board;
+import com.blog.domain.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BoardRepository extends JpaRepository<Board,Long> {
+    List<Board> findAllByUserOrderByIdDesc(User user);
+    Page<Board> findByCategory(Pageable pageable, String category);
 }
