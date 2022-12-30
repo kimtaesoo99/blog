1f0668b00197a04cc946f89727bf1561b2f648d5
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-12-30 16:16:17 +0900

    feat: (PrincipalDetail) PrincipalDetail 구현

---
+++ src/main/java/com/blog/config/auth/PrincipalDetail.java
@@ -0,0 +1,58 @@
+package com.blog.config.auth;
+
+import com.blog.domain.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+//스프리 시큐리티가 로그인 요청을 가로채서 로그인을 진행하고 완료가 되면 UserDetail타입의 옵젝을
+// 스프링 시큐리티의 고유한 세션저장소에 저장함
+public class PrincipalDetail implements UserDetails {
+
+    private User user; // 컴포지션
+
+    public PrincipalDetail(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getPassword();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collection = new ArrayList<>();
+        collection.add(() -> { return "ROLE_" + user.getRole();});
+
+
+        return collection;
+    }
+}
