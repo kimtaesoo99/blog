3dc44a58fd4b90b507c168764e4f4ce183f4fe5d
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-12-30 16:19:55 +0900

    feat: (UserService) 비밀번호 암호화기능 추가

--- src/main/java/com/blog/service/UserService.java
+++ src/main/java/com/blog/service/UserService.java
@@ -1,8 +1,10 @@
 package com.blog.service;
 
+import com.blog.domain.ROLETYPE;
 import com.blog.domain.User;
 import com.blog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,8 +15,15 @@ public class UserService {
     private UserRepository userRepository;
 
 
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
     @Transactional
     public void save(User user) {
+        String rawPassword = user.getPassword();
+        String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+        user.setPassword(encPassword);
+        user.setRole(ROLETYPE.USER);
         userRepository.save(user);
     }
 }
