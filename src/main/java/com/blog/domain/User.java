4a24d3bf295581731d9b35e30b20634fd6fe10f9
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-12-30 22:16:15 +0900

    feat: (User) 카카오 로그인여부 체크

--- src/main/java/com/blog/domain/User.java
+++ src/main/java/com/blog/domain/User.java
@@ -4,37 +4,46 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.ColumnDefault;
-import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
-import java.sql.Timestamp;
+import java.time.LocalDate;
 
-//ORM -> 오브젝트를 매핑해줌
-@Entity //User클래스가 MYSQL 테이블이 생성된다.
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Entity
+//@DynamicInsert // Insert시에 null인 필드를 제외시킴
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // 프로젝트에 연결된 DB의 넘버링 전략을 따라감
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false,length = 30)
-    private String userName;
+    @Column(nullable = false, length = 100, unique = true)
+    private String username; // 아이디
 
-    @Column(nullable = false,length = 100) // -> 해쉬 (비밀번호 암호화)
+    @Column(nullable = false, length = 100)
     private String password;
 
-    @Column(nullable = false,length = 50)
+    @Column(nullable = false, length = 50)
     private String email;
 
+    //    @ColumnDefault("user")
+    // DB는 RoleType이라는게 없어서, String임을 알려준다.
     @Enumerated(EnumType.STRING)
-    private  ROLETYPE role; //Enum을 쓰는게 좋음.
+    private RoleType role; // Enum을 쓰면, ADMIN,USER로 형이 강제가 된다.
 
-    @CreationTimestamp //시간이 자동 입력
-    private Timestamp createDate;
+    private String oauth; // kakao, google ... 회원수정을 막기위해 필요함
 
+
+
+    @DateTimeFormat(pattern = "yyyy-mm-dd")
+    private LocalDate createDate; // 날짜
+
+    @PrePersist // DB에 INSERT 되기 직전에 실행. 즉 DB에 값을 넣으면 자동으로 실행됨
+    public void createDate() {
+        this.createDate = LocalDate.now();
+    }
 }
