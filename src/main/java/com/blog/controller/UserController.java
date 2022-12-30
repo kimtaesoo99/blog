47cc562fb8650b12295411337236e925165f9abb
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-12-30 22:15:26 +0900

    feat: (UserController) 유저  컨트롤러 구현

--- src/main/java/com/blog/controller/UserController.java
+++ src/main/java/com/blog/controller/UserController.java
@@ -1,8 +1,30 @@
 package com.blog.controller;
 
+import com.blog.config.auth.PrincipalDetail;
+import com.blog.domain.KakaoProfile;
+import com.blog.domain.OAuthToken;
+import com.blog.domain.User;
+import com.blog.service.BoardService;
+import com.blog.service.UserService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
 
 // 인증이 안된 사용자들이 출입할 수 있는 경로
 // /auth/** 허용
@@ -11,6 +33,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
+
+    @Value("${root.key}")
+    private String rootKey;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private BoardService boardService;
 
 
     @GetMapping("/auth/joinForm")
@@ -22,5 +56,130 @@ public class UserController {
     @GetMapping("/auth/loginForm")
     public String loginForm() {
         return "user/loginForm";
+    }
+
+
+    // 카카오
+    @GetMapping("/auth/kakao/callback")
+    public String kakaoCallback(String code) {
+        // @ResponseBody는 Data를 리턴해주는 컨트롤러 함수가 됨
+
+
+        // POST방식으로 Key=Value 데이터를 요청 (카카오쪽으로)
+        RestTemplate rt = new RestTemplate();
+
+        // HttpHeader 오브젝트 생성
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        // HttpBody 오브젝트 생성
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "authorization_code");
+        params.add("client_id", "19bc8b342e4af553e0ec5456a5c82bde");
+        params.add("redirect_uri", "http://localhost:8080/auth/kakao/callback");
+        params.add("code", code);
+
+        // HttpHeader와 HttpBody를 하나의 오브젝트에 담기
+        HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest =
+            new HttpEntity<>(params, headers);
+
+        // Http 요청하기 - POST방식으로 - 그리고 response 변수의 응답 받음
+        ResponseEntity<String> response = rt.exchange(
+            "https://kauth.kakao.com/oauth/token",
+            HttpMethod.POST,
+            kakaoTokenRequest,
+            String.class
+        );
+
+        // Gson, Json Simple, ObjectMapper
+        ObjectMapper objectMapper = new ObjectMapper();
+        OAuthToken oAuthToken = null;
+
+        try {
+            oAuthToken = objectMapper.readValue(response.getBody(), OAuthToken.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+
+        // POST방식으로 Key=Value 데이터를 요청 (카카오쪽으로)
+        RestTemplate rt2 = new RestTemplate();
+
+        // HttpHeader 오브젝트 생성
+        HttpHeaders headers2 = new HttpHeaders();
+        headers2.add("Authorization", "Bearer " + oAuthToken.getAccess_token());
+        headers2.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+
+        // HttpHeader와 HttpBody를 하나의 오브젝트에 담기
+        HttpEntity<MultiValueMap<String, String>> kakaoProfileRequest =
+            new HttpEntity<>(headers2);
+
+        // Http 요청하기 - POST방식으로 - 그리고 response 변수의 응답 받음
+        ResponseEntity<String> response2 = rt2.exchange(
+            "https://kapi.kakao.com/v2/user/me",
+            HttpMethod.POST,
+            kakaoProfileRequest,
+            String.class
+        );
+
+        System.out.println(response2.getBody());
+
+
+        // Gson, Json Simple, ObjectMapper
+        ObjectMapper objectMapper2 = new ObjectMapper();
+        KakaoProfile kakaoProfile = null;
+
+        try {
+            kakaoProfile = objectMapper2.readValue(response2.getBody(), KakaoProfile.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        // User 오브젝트 : username, password, email
+        System.out.println("카카오 아이디(번호) : " + kakaoProfile.getId());
+        System.out.println("카카오 이메일 : " + kakaoProfile.getKakao_account().getEmail());
+        System.out.println("블로그서버 유저네임 : " + kakaoProfile.getKakao_account().getEmail() + "_" + kakaoProfile.getId());
+        System.out.println("블로그서버 이메일 : " + kakaoProfile.getKakao_account().getEmail());
+        System.out.println("블로그서버 패스워드 : " + rootKey);
+
+        User kakaoUser = User.builder()
+            .username(kakaoProfile.getKakao_account().getEmail() + "_" + kakaoProfile.getId())
+            .password(rootKey)
+            .email(kakaoProfile.getKakao_account().getEmail())
+            .oauth("kakao")
+            .build();
+
+
+        // 가입자 혹은 비가입자 체크해서 처리
+        User originUser = userService.회원찾기(kakaoUser.getUsername());
+        if (originUser.getUsername() == null) {
+            System.out.println("기존 회원이 아니기에 자동 회원가입을 진행합니다.");
+            userService.회원가입(kakaoUser);
+        }
+
+        System.out.println("자동 로그인을 진행합니다.");
+        // 로그인 처리 및 세션처리
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(kakaoUser.getUsername(), rootKey));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+
+        return "redirect:/";
+    }
+
+
+    @GetMapping("/user/updateForm")
+    public String updateForm(Model model, @AuthenticationPrincipal PrincipalDetail principalDetail) {
+        model.addAttribute("user", principalDetail.getUser());
+        return "/user/updateForm";
+    }
+
+
+    // 작성글 보기
+    @GetMapping("/user/myBoard")
+    public String index(Model model, @AuthenticationPrincipal PrincipalDetail principalDetail) {
+        model.addAttribute("user", principalDetail.getUser());
+        model.addAttribute("boards", boardService.작성글목록(principalDetail.getUser()));
+        return "/user/myBoard";
     }
 }
