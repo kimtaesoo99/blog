c6ae753a165a780a7b803612c72b1205fb7a5de8
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-12-30 16:19:11 +0900

    feat: (UserRepository) 유저이름으로 찾는 기능 추가

--- src/main/java/com/blog/repository/UserRepository.java
+++ src/main/java/com/blog/repository/UserRepository.java
@@ -4,8 +4,11 @@ import com.blog.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
     //SELECT * FROM user WHERE username =?1 AND password =?2;
     //User findByUserNameAndPassword(String username,String password);
+    Optional<User> findByUsername(String username);
 }
